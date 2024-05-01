6e07f683e86d83fbdcfe597c29fe78052dd86f44
mohammedajmalgit <mdajmalmt@gmail.com>
2024-05-01 17:45:52 +0530

    mail on updates

--- controller/majlisController.js
+++ controller/majlisController.js
@@ -1,11 +1,21 @@
-import { sendEmailToAdmin, sendConfirmationEmail } from '../services/emailService.js';
+import { sendEmailToAdmin, sendConfirmationEmail, sendUpdateEmail } from '../services/emailService.js';
 import db from '../firebase.js';
+
+const generateBookingNumber = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let bookingNumber = '';
+  for (let i = 0; i < 8; i++) {
+    bookingNumber += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return bookingNumber;
+};
 
 export const createMajlisBooking = async (req, res) => {
   try {
     const formData = req.body;
-    const id = req.body.email;
+    const id = generateBookingNumber();
     const userJson = {
+      id: id,
       email: req.body.email,
       name: req.body.name,
       date: req.body.date,
@@ -18,9 +28,9 @@ export const createMajlisBooking = async (req, res) => {
     const response = db.collection('reservation').doc(id).set(userJson)
     console.log(response);
     // Send email to admin
-    sendEmailToAdmin(formData, "New Majlis Booking");
+    sendEmailToAdmin(formData, id, "New Majlis Booking");
     // Send confirmation email to user
-    sendConfirmationEmail(formData);
+    sendConfirmationEmail(formData, id);
     res.status(201).send(formData);
   } catch (error) {
     return res.status(500).send({ message: error.message })
@@ -57,6 +67,9 @@ export const cancelMajlisReservation = async (req, res) => {
       .update({
         status: "cancelled"
       })
+    const user = db.collection("reservation").doc(req.params.id)
+    const response = await user.get()
+    sendUpdateEmail(response.data(), "cancelled", req.params.id)
     res.send(userRef)
   } catch (error) {
     res.send(error)
@@ -65,12 +78,16 @@ export const cancelMajlisReservation = async (req, res) => {
 
 export const updateMajlisReservation = async (req, res) => {
   try {
-    // console.log(req)
     const userRef = db.collection("reservation").doc(req.params.id)
       .update({
         status: req.body.status
       })
-    res.send(userRef)
+    const user = db.collection("reservation").doc(req.params.id)
+    const response = await user.get()
+    if (req.body.status !== 'completed') {
+      sendUpdateEmail(response.data(), req.body.status, req.params.id)
+    }
+    res.send(response.data())
   } catch (error) {
     res.send(error)
   }
@@ -79,6 +96,9 @@ export const updateMajlisReservation = async (req, res) => {
 export const deleteMajlisReservation = async (req, res) => {
   try {
     const response = await db.collection("reservation").doc(req.params.id).delete()
+    // const user = db.collection("reservation").doc(req.params.id)
+    // const response2 = await user.get()
+    // sendUpdateEmail(response2.data(), "delete", req.params.id)
     res.send(response)
   } catch (error) {
     res.send(error)
--- services/emailService.js
+++ services/emailService.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer'
 
-export function sendEmailToAdmin(formData, subject) {
+export function sendEmailToAdmin(formData, id, subject) {
   const transporter = nodemailer.createTransport({
     host: 'smtp.zoho.in',
     port: 587,
@@ -15,17 +15,25 @@ export function sendEmailToAdmin(formData, subject) {
     from: 'leicester@khaleejmandi.co.uk',
     to: 'leicester@khaleejmandi.co.uk',
     subject: subject,
-    html: `<p>Name: ${formData.name}</p>
-    <p>Email: ${formData.email}</p>
-    <p>Phone: ${formData.phoneNumber}</p>
-    ${formData.date ? `
-      <div>
-        <p>Date: ${formData.date}</p>
-        <p>Time: ${formData.time}</p>
-        <p>No. of Persons: ${formData.numberOfPersons}</p>
-      </div>` : ''}
-    <p>Message: ${formData.message}</p>
-    ...`, // Add other form fields
+    html: `
+    <div style="max-width: 600px; margin: 20px auto; padding: 20px; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
+        <h1 style="color: #333333; margin-bottom: 20px;">New Majlis Booking Received</h1>
+        <div style="background-color: #f9f9f9; padding: 20px; border-radius: 5px; margin-bottom: 20px;">
+            <p style="color: #666666; margin: 5px 0;"><strong>Booking number:</strong> ${id}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Name:</strong> ${formData.name}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Email:</strong> ${formData.email}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Phone:</strong> ${formData.phoneNumber}</p>
+            ${formData.date ? `
+            <div>
+              <p style="color: #666666; margin: 5px 0;"><strong>Date:</strong> ${formData.date}</p>
+              <p style="color: #666666; margin: 5px 0;"><strong>Time:</strong> ${formData.time}</p>
+            </div>`
+        :
+        ''}
+            <p style="color: #666666; margin: 5px 0;"><strong>No. of Persons:</strong> ${formData.numberOfPersons}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Message:</strong> ${formData.message}</p>
+        </div>
+    </div>`, // Add other form fields
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -37,7 +45,7 @@ export function sendEmailToAdmin(formData, subject) {
   });
 }
 
-export function sendConfirmationEmail(formData) {
+export function sendConfirmationEmail(formData, id) {
   const transporter = nodemailer.createTransport({
     // service: 'gmail',
     // auth: {
@@ -56,15 +64,72 @@ export function sendConfirmationEmail(formData) {
   const mailOptions = {
     from: 'leicester@khaleejmandi.co.uk',
     to: formData.email,
-    subject: 'Majlis Booking Received',
-    html: `<p>Name: ${formData.name}</p>
-    <p>Email: ${formData.email}</p>
-    <p>Phone: ${formData.phoneNumber}</p>
-    <p>Date: ${formData.date}</p>
-    <p>Time: ${formData.time}</p>
-    <p>No. of Persons: ${formData.numberOfPersons}</p>
-    <p>Message: ${formData.message}</p>
-    ...`, // Add other form fields
+    subject: `Majlis Booking Received`,
+    html: `
+      <div style="max-width: 600px; margin: 20px auto; padding: 20px; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);" >
+        <div style="text-align: center; margin-bottom: 20px;">
+            <img src="https://www.khaleejmandi.co.uk/Logo.png" alt="Restaurant Logo" style="width: auto; max-height: 150px;  margin-left: auto; margin-right: auto;">
+        </div>
+        <h1 style="color: #333333; margin-bottom: 20px;">Majlis Booking Received</h1>
+        <div style="background-color: #f9f9f9; padding: 20px; border-radius: 5px; margin-bottom: 20px;">
+            <p style="color: #666666; margin: 5px 0;"><strong>Booking number:</strong> ${id}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Name:</strong> ${formData.name}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Email:</strong> ${formData.email}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Phone:</strong> ${formData.phoneNumber}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Date:</strong> ${formData.date}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Time:</strong> ${formData.time}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>No. of Persons:</strong> ${formData.numberOfPersons}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Message:</strong> ${formData.message}</p>
+        </div>
+        <p style="color: #666666; margin-bottom: 20px;">Thank you for choosing our restaurant for your reservation. We look forward to serving you!</p>
+        <img src="https://www.khaleejmandi.co.uk/hmc%20black.png" alt="Image" style="max-width: 100px; height: auto;">
+      <div/>`, // Add other form fields
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
+export function sendUpdateEmail(formData, update, id) {
+  console.log(formData);
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.zoho.in',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'leicester@khaleejmandi.co.uk',
+      pass: 'HWpvqLKuVEid',
+    },
+  });
+
+  const mailOptions = {
+    from: 'leicester@khaleejmandi.co.uk',
+    to: formData.email,
+    subject: `Majlis booking ${update}`,
+    html: `
+      <div style="max-width: 600px; margin: 20px auto; padding: 20px; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);" >
+        <div style="text-align: center; margin-bottom: 20px;">
+            <img src="https://www.khaleejmandi.co.uk/Logo.png" alt="Restaurant Logo" style="width: auto; max-height: 150px;  margin-left: auto; margin-right: auto;">
+        </div>
+        <h1 style="color: #333333; margin-bottom: 20px;">Majlis booking ${update}</h1>
+        <div style="background-color: #f9f9f9; padding: 20px; border-radius: 5px; margin-bottom: 20px;">
+            <p style="color: #666666; margin: 5px 0;"><strong>Booking number:</strong> ${id}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Name:</strong> ${formData.name}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Email:</strong> ${formData.email}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Phone:</strong> ${formData.phoneNumber}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Date:</strong> ${formData.date}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Time:</strong> ${formData.time}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>No. of Persons:</strong> ${formData.numberOfPersons}</p>
+            <p style="color: #666666; margin: 5px 0;"><strong>Message:</strong> ${formData.message}</p>
+        </div>
+        <p style="color: #666666; margin-bottom: 20px;">Thank you for choosing our restaurant for your reservation. We look forward to serving you!</p>
+        <img src="https://www.khaleejmandi.co.uk/hmc%20black.png" alt="Image" style="max-width: 100px; height: auto;">
+      <div/>`, // Add other form fields
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
